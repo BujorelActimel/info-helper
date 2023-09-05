d24f46649b06cc7b6994589f8864331d34a8c019
bujor <bujormihaialexandru@gmail.com>
2023-09-05 21:47:51 +0300

    some ui work

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,23 +1,35 @@
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
-import Image from "next/image"
-
+import Image from "next/image";
 
 export default function Landing() {
   return (
     <>
-      <h1 className="text-white text-4xl font-bold text-center p-4 my-2">Landing (Oricine)</h1>
+      <h1 className="text-white text-6xl font-bold text-center p-4 my-2">
+        Landing (Oricine)
+      </h1>
+
       <div className="flex items-center justify-center p-4">
         <Link href='/sign-in'>
-          <Button className="m-4">Login</Button>
+          <Button className="m-4">
+            Login
+          </Button>
         </Link>
+
         <Link href='/sign-up'>
-          <Button className="m-4">Register</Button>
+          <Button className="m-4">
+            Register
+          </Button>
         </Link>
       </div>
-      <div className="flex justify-end mx-24">
-        <Image src="/output.png" alt="laptop" width={550} height={550}/>
-      </div>
+
+      <Image
+        src='/hero-final.jpg'
+        alt='hero'
+        width={1000}
+        height={1000}
+        quality={100}
+      />
     </>
-  )
+  );
 }
